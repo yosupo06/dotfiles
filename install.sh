399f57e0484694a1afc80fbb5e5ab1da95f3dbcb
Kohei Morita <yosupo@localhost.localdomain>
2017-08-27 22:21:01 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 
-echo '.' $THIS_DIR'/profile' >> ~/.bash_profile #install bash_profile
+if [ -e ~/.profile ]; then
+    echo '.' $THIS_DIR'/profile' >> ~/.profile #install bash_profile
+else
+    echo '.' $THIS_DIR'/profile' >> ~/.bash_profile #install bash_profile
+fi
+
 echo '.' $THIS_DIR'/bashrc' >> ~/.bashrc #install bashrc
 cp -r $THIS_DIR'/emacs.d' ~/.emacs.d
 
