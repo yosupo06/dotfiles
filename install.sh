93a92888c29ced845ca28a9a057a71d1d71d33f0
Kohei Morita <yosupo06@gmail.com>
2024-02-25 23:21:55 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Kohei Morita"
+git config --global user.email yosupo06@gmail.com
+
+git config --global alias.st status
+git config --global alias.cob "checkout -b"
+git config --global alias.view "log --oneline --decorate --graph --branches --tags"
