68830f30d91fd69f94481580d32531762b178139
Kohei Morita <yosupo06@gmail.com>
2024-05-12 04:12:48 +0900

    upd

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ ansible-playbook wsl.yml --ask-become-pass -v
 
 WSL verified
 OS X ???
+
+git config --global user.name "Kohei Morita"
+git config --global user.email yosupo06@gmail.com
+
--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-git config --global user.name "Kohei Morita"
-git config --global user.email yosupo06@gmail.com
-
 git config --global alias.st status
 git config --global alias.cob "checkout -b"
 git config --global alias.view "log --oneline --decorate --graph --branches --tags"
