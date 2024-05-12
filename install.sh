07ea90ee8c505d1bea968e94de53af10abd29757
Kohei Morita <yosupo06@gmail.com>
2024-05-12 15:47:40 +0900

    remove git config

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-git config --global alias.st status
-git config --global alias.cob "checkout -b"
-git config --global alias.view "log --oneline --decorate --graph --branches --tags"
+#!/usr/bin/env bash
+
