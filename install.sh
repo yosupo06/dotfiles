be6aa6b84522eb623d0e26e3d3e7820799ec2dbc
Kohei Morita <yosupo06@gmail.com>
2024-06-18 18:48:07 +0900

    add git alias

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ cp oh-my-zsh/custom/alias.zsh $HOME/.oh-my-zsh/custom
 if [ "$REMOTE_CONTAINERS" != "true" ]; then
     git config --global user.name "Kohei Morita"
     git config --global user.email "yosupo06@gmail.com"
+
+    git config --global alias.st "status"
+    git config --global alias.cob "checkout -b"
 fi
