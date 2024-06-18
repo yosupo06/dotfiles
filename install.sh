e0cef232d8f04911005d35eda5dfdf9006b0da43
Kohei Morita <yosupo06@gmail.com>
2024-06-18 18:41:22 +0900

    upd install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 cp oh-my-zsh/custom/alias.zsh $HOME/.oh-my-zsh/custom
+
+if [ "$REMOTE_CONTAINERS" != "true" ]; then
+    git config --global user.name "Kohei Morita"
+    git config --global user.email "yosupo06@gmail.com"
+fi
